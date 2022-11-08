a38646ac4331c5eb12a77765e5c1206742fbe6fa
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-11-08 17:39:07 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+// import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Restaurant from "./pages/Restaurant";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        {/* <Route path="/room-detail/:roomSlug" element={<SingleRoom />} /> */}
+        <Route path="/restaurant" element={<Restaurant />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,139 @@
+import restaurantImage from "./statics/slider-02.jpg";
+import roomImage from "./statics/room-2.jpg";
+
+import img1 from "./statics/img-01.jpg";
+import img2 from "./statics/item-6.jpeg";
+import img3 from "./statics/item-5.jpeg";
+import img4 from "./statics/item-2.jpeg";
+import img5 from "./statics/item-8.jpeg";
+import img6 from "./statics/item-4.jpeg";
+import img7 from "./statics/item-7.jpeg";
+import img8 from "./statics/item-1.jpeg";
+import img9 from "./statics/img-02.jpg";
+import img10 from "./statics/img-03.jpg";
+import img11 from "./statics/img-07.jpg";
+import img12 from "./statics/img-09.jpg";
+import img13 from "./statics/img-08.jpg";
+import img14 from "./statics/img-05.jpg";
+import img15 from "./statics/img-04.jpg";
+import img16 from "./statics/menu-5.jpg";
+
+export const menuData = [
+  {
+    id: 16,
+    image: img16,
+    title: "Lamb Tomato",
+    price: 28,
+    category: "Dinner",
+  },
+  {
+    id: 1,
+    image: img1,
+    title: "Godzilla Milkshake",
+    price: 25,
+    category: "Drinks",
+  },
+  {
+    id: 2,
+    image: img4,
+    title: "Diner Double",
+    price: 15,
+    category: "Lunch",
+  },
+  {
+    id: 13,
+    image: img13,
+    title: "Potato Cheese",
+    price: 15,
+    category: "Dinner",
+  },
+  {
+    id: 3,
+    image: img9,
+    title: "Quarantine Buddy",
+    price: 10,
+    category: "Drinks",
+  },
+  {
+    id: 14,
+    image: img14,
+    title: "Vegetable Stir-fry",
+    price: 15,
+    category: "Dinner",
+  },
+  {
+    id: 4,
+    image: img6,
+    title: "Country Delight",
+    price: 15,
+    category: "Lunch",
+  },
+  {
+    id: 15,
+    image: img15,
+    title: "Burger Perhaps",
+    price: 15,
+    category: "Lunch",
+  },
+  {
+    id: 5,
+    image: img3,
+    title: "Egg Attack",
+    price: 10,
+    category: "Lunch",
+  },
+  {
+    id: 10,
+    image: img10,
+    title: "Fancy Gulp",
+    price: 13,
+    category: "Drinks",
+  },
+  {
+    id: 6,
+    image: img2,
+    title: "Oreo Dream",
+    price: 12,
+    category: "Drinks",
+  },
+  {
+    id: 7,
+    image: img7,
+    title: "Bacon Overflow",
+    price: 15,
+    category: "Lunch",
+  },
+  {
+    id: 8,
+    image: img5,
+    title: "American Classic",
+    price: 18,
+    category: "Lunch",
+  },
+  {
+    id: 12,
+    image: img12,
+    title: "Pork Chops",
+    price: 13,
+    category: "Dinner",
+  },
+  {
+    id: 9,
+    image: img8,
+    title: "Buttermilk Pancakes",
+    price: 13,
+    category: "Lunch",
+  },
+  {
+    id: 11,
+    image: img11,
+    title: "Baked Chicken",
+    price: 13,
+    category: "Dinner",
+  },
+];
+
+export const hero = {
+  restaurant: restaurantImage,
+  room: roomImage,
+};

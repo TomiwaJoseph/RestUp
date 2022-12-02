cd5fd9eb7a11655756247a2a1eb815eb7c33b71b
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-02 11:43:18 +0100

    added models to admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Apartment, Room, ApartmentImages, RoomInfo, RoomExtra
 
-# Register your models here.
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name', 'max_people', 'price', 'bed_type',
+                    'size', 'refundable', 'availability']
+    prepopulated_fields = {'slug': ('name',)}
+    # list_editable = ['discount_price', 'availability']
+    # list_filter = ['category']
+    # search_fields = ['name', 'price']
+
+
+admin.site.register(Apartment, ApartmentAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(ApartmentImages)
+admin.site.register(RoomInfo)
+admin.site.register(RoomExtra)

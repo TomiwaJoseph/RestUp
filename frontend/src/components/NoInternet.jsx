110a08286c11c133e5ca4428871bc68db754b858
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-04 20:38:31 +0100

    added an internet connection error page

--- frontend/src/components/NoInternet.jsx
+++ frontend/src/components/NoInternet.jsx
@@ -1,14 +1,14 @@
-import errorImg from "../statics/404 Error Page not Found with people connecting a plug-bro.png";
+import errorImg from "../statics/no-connection-illustration-concept-vector.jpg";
 
 const NoInternet = () => {
   return (
-    <div className="error__div">
-      <div className="error_image_wrapper">
+    <div className="error-div">
+      <div className="error-image-wrapper">
         <img src={errorImg} alt="server-error" className="img-fluid" />
       </div>
       <h1>An error occurred...</h1>
       <p>Check your internet connection and try again.</p>
-      <button onClick={() => window.location.reload()} className="refresh__btn">
+      <button onClick={() => window.location.reload()} className="refresh-btn">
         Refresh Page
       </button>
     </div>

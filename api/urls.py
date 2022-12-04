9777325d780e79158fe73029cc69cbbe1ac4651e
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-04 20:42:12 +0100

    added more api routes

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
     path('test-page/', views.test_page),
+    path('apartments/', views.get_apartments),
+    path('filtered-apartments/', views.filtered_apartments),
     path('featured-apartments/', views.get_featured_apartments),
     path('highest-price-and-capacity/', views.get_highest_price_and_capacity),
     path('apartment/<slug:slug>/', views.get_single_apartment),
-    path('filter-apartment/', views.filter_apartments),
     path('save-stripe-info/', views.save_stripe_info),
 ]

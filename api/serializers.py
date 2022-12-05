0c99dc6026a70c5417dca0dc913e99171e8a9827
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-05 23:42:25 +0100

    added room serializer to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from base.models import Apartment, Room, ApartmentImages
+from base.models import Apartment, Room
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
@@ -8,7 +8,17 @@ class ApartmentSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'slug', 'main_image']
 
 
-# class RoomSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Room
-#         fields = ['id', 'name', 'slug', 'main_image']
+class RoomSerializer(serializers.ModelSerializer):
+    room_info = serializers.SerializerMethodField()
+    room_extras = serializers.SerializerMethodField()
+
+    def get_room_info(self, obj):
+        return obj.room_info.all().values_list('info', flat=True)
+
+    def get_room_extras(self, obj):
+        return obj.room_extras.all().values_list('extra', flat=True)
+
+    class Meta:
+        model = Room
+        fields = ['id', 'name', 'slug', 'max_people', 'price',
+                  'bed_type', 'size', 'refundable', 'room_info', 'room_extras']

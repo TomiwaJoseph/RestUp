db4bc2c48ce47863baf33c1fbb67c14bb234e9e2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-12 22:21:46 +0100

    first commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="login-div">
+        <h1>Testing</h1>
+      </div>
+    </>
+  );
+};
+
+export default Login;

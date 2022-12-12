0652c54cbf563ce2367ccfa9ed7f74401c622afe
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-12 22:21:58 +0100

    first commit

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <>
+      <div className="signup-div">
+        <h1>Testing</h1>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;

f15035a52e5384048df18ff27835225982f8672e
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-14 11:33:44 +0100

    first commit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="dashboard-container">
+        <h1>Dashboard</h1>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;

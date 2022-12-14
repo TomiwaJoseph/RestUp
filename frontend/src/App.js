f91188812bbe3159ea52c2c9baaf9bb00485e56f
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-14 19:26:09 +0100

    added login, signup, errorpage and dashboard to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,10 @@ import Footer from "./components/Footer";
 import SingleApartment from "./pages/SingleApartment";
 import ReserveRoom from "./pages/ReserveRoom";
 import Search from "./pages/Search";
+import ErrorPage from "./components/ErrorPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -26,8 +30,12 @@ const App = () => {
         <Route path="/reserve-room/:roomSlug" element={<ReserveRoom />} />
         <Route path="/restaurant" element={<Restaurant />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user/dashboard" element={<DashBoard />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/contact-us" element={<Contact />} />
         <Route path="/search-apartments" element={<Search />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </Router>

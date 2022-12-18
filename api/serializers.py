ed40ccdf7e38d038cf548345d53f069a1f4975dd
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-18 15:06:49 +0100

    added registerSerializer to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from base.models import Apartment, Room
+
+User = get_user_model()
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
@@ -22,3 +25,32 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ['id', 'name', 'slug', 'max_people', 'price',
                   'bed_type', 'size', 'refundable', 'room_info', 'room_extras']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name',
+                  'email', 'password']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def validate_email(self, email):
+        existing_emails = User.objects.filter(email=email).first()
+        if existing_emails:
+            raise serializers.ValidationError(
+                "User with this email already exist. Wasn't you?")
+        return email

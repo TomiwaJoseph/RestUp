2ff23833610350aee17cc41ae6936658d62f21e2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2022-12-22 14:30:22 +0100

    added booking model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import CustomUser
+from .models import CustomUser, Booking
 
 
 @admin.register(CustomUser)
@@ -22,3 +22,14 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email', 'first_name', 'last_name')
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'room', 'occupant_email',
+                    'stay_duration',  'start_date', 'end_date']
+    # list_editable = ['start_date', 'end_date']
+    # list_filter = ['category']
+    # search_fields = ['name', 'price']
+
+
+admin.site.register(Booking, BookingAdmin)

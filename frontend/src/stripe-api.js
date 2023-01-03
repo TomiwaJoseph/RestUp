7f14e9efbb3c31bc3bf7e101087a3c79796ea7bc
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-03 23:33:53 +0100

    implemented stripe api call logic

---
+++ frontend/src/stripe-api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_ENDPOINT = "http://localhost:8000/api";
+
+export const stripePaymentMethodHandler = async (data) => {
+  const { amount, userInfo, stayDuration, roomApartmentSlug, result } = data;
+  let body = JSON.stringify({
+    payment_method_id: result.id,
+    name: result.billing_details.name,
+    email: result.billing_details.email,
+    amount: amount,
+    userInfo: userInfo,
+    stayDuration: stayDuration,
+    roomApartmentSlug: roomApartmentSlug,
+    token: localStorage.getItem("restupToken"),
+  });
+  const res = await axios.post(`${API_ENDPOINT}/save-stripe-info/`, body, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};

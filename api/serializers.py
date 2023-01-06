68b7eb915701de13f2837b7ea3d3e0345fcb34f1
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-06 15:02:05 +0100

    added BookingSerializer to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,9 @@
-from rest_framework import serializers
+from datetime import datetime, time
+from django.utils import timezone
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from base.models import Apartment, Room
+from users.models import Booking
 
 User = get_user_model()
 
@@ -54,3 +57,51 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "User with this email already exist. Wasn't you?")
         return email
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    # room_refundable = serializers.SerializerMethodField()
+    # room_price = serializers.SerializerMethodField()
+    room_name = serializers.SerializerMethodField()
+    room_apartment = serializers.SerializerMethodField()
+    start_date = serializers.SerializerMethodField()
+    end_date = serializers.SerializerMethodField()
+    spent = serializers.SerializerMethodField()
+    show_cancel = serializers.SerializerMethodField()
+
+    def get_show_cancel(self, obj):
+        if not obj.room.refundable:
+            return False
+        elif obj.end_date < timezone.now():
+            return False
+        elif timezone.now().time() >= time(23, 59):
+            return False
+        return True
+
+    # def get_room_refundable(self, obj):
+    #     return timezone.now().time() <= time(23, 59)
+
+    def get_spent(self, obj):
+        return obj.end_date < timezone.now()
+
+    def get_room_name(self, obj):
+        return obj.room.name
+
+    def get_room_apartment(self, obj):
+        return obj.room.apartment.name
+
+    def get_start_date(self, obj):
+        return obj.start_date.strftime('%a, %b %d, %Y')
+
+    def get_end_date(self, obj):
+        return obj.end_date.strftime('%a, %b %d, %Y')
+
+    class Meta:
+        model = Booking
+        fields = [
+            'id', 'occupant_first_name', 'ref_code',
+            'occupant_last_name', 'occupant_email',
+            'occupant_phone_number', 'stay_duration',
+            'start_date', 'end_date',
+            'room_name', 'room_apartment', 'spent', 'show_cancel'
+        ]

5d4f93ce555f5550b2881c254ef377399ecd8829
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-06 15:10:39 +0100

    changed stripe submit form logic

--- frontend/src/components/CheckoutForm.jsx
+++ frontend/src/components/CheckoutForm.jsx
@@ -6,14 +6,15 @@ import {
   CardExpiryElement,
   CardCvcElement,
 } from "@stripe/react-stripe-js";
-import { stripePaymentMethodHandler } from "../stripe-script";
+import { stripePaymentMethodHandler } from "../stripe-api";
 import "./checkoutform.css";
+import { toast } from "react-toastify";
 
 const CARD_ELEMENT_OPTIONS = {
   style: {
     base: {
       color: "#212529",
-      fontSize: "16px",
+      fontSize: "15px",
       "::placeholder": {
         color: "#aab7c4",
       },
@@ -28,58 +29,76 @@ const CARD_ELEMENT_OPTIONS = {
 const CheckoutForm = (props) => {
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const notify = (message, errorType) =>
+    toast(message, {
+      position: "top-center",
+      autoClose: "3000",
+      pauseOnHover: true,
+      closeOnClick: true,
+      type: errorType,
+      theme: "colored",
+    });
 
   const stripe = useStripe();
   const elements = useElements();
   const handlePaymentFormSubmit = async (event) => {
-    event.preventDefault();
+    try {
+      event.preventDefault();
+      if (!stripe || !elements) {
+        // Stripe.js has not yet loaded.
+        // Make sure to disable form submission until Stripe.js has loaded.
+        return;
+      }
 
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
+      setLoading(true);
+      setErrorMsg("");
+      let name = props.userInfo[0] + " " + props.userInfo[1];
+      let email = props.userInfo[2];
+      let phone = props.userInfo[3];
+
+      const { paymentMethod, error } = await stripe.createPaymentMethod({
+        type: "card",
+        card: elements.getElement(CardNumberElement),
+        billing_details: {
+          name,
+          email,
+          phone,
+        },
+      });
+
+      // console.log("Here is the payment method...");
+      // console.log(paymentMethod);
+      // console.log(" ");
+
+      if (error) {
+        setLoading(false);
+        setErrorMsg(error.message);
+      } else {
+        // console.log("Sending details to server...");
+        stripePaymentMethodHandler({
+          amount: props.amount,
+          userInfo: props.userInfo,
+          stayDuration: props.stayDuration,
+          roomApartmentSlug: props.roomApartmentSlug,
+          result: paymentMethod,
+        })
+          .then((response) => {
+            // console.log(response);
+            // console.log(response.data.message);
+            props.setPaymentCompleted(
+              response.data.message === "Success" ? true : false
+            );
+          })
+          .catch((err) => {
+            setLoading(false);
+            setErrorMsg(err.response.data.error);
+          });
+      }
+    } catch (err) {
+      setLoading(false);
+      setErrorMsg("Network communication failed, try again.");
+      notify("Your internet connection is bad. Try again later.", "info");
     }
-
-    setLoading(true);
-    setErrorMsg("");
-    let name = props.userInfo[0] + " " + props.userInfo[1];
-    let email = props.userInfo[2];
-
-    const paymentMethodObj = {
-      type: "card",
-      card: elements.getElement(CardNumberElement),
-      billing_details: {
-        name,
-        email,
-      },
-    };
-    const paymentMethodResult = await stripe.createPaymentMethod(
-      paymentMethodObj
-    );
-
-    stripePaymentMethodHandler(
-      {
-        amount: props.amount,
-        userInfo: props.userInfo,
-        stayDuration: props.stayDuration,
-        roomApartmentSlug: props.roomApartmentSlug,
-        result: paymentMethodResult,
-      },
-      handleResponse
-    );
-  };
-  // callback method to handle the response
-  const handleResponse = (response) => {
-    setLoading(false);
-    if (response.error) {
-      setErrorMsg(
-        typeof response.error === "string"
-          ? response.error
-          : response.error.message
-      );
-      return;
-    }
-    props.setPaymentCompleted(response.message === "Success" ? true : false);
   };
 
   return (
@@ -113,17 +132,15 @@ const CheckoutForm = (props) => {
             />
           </div>
         </div>
-        <hr className="mb-4" />
+        <hr className="mb-3 mt-1" />
         <button className="btn btn-dark w-100" type="submit" disabled={loading}>
           {loading ? (
-            <div className="spinner-border text-light" role="status">
-              {/* <span className="sr-only">Loading...</span> */}
-            </div>
+            <div className="spinner-border text-light" role="status"></div>
           ) : (
             `Pay $${props.amount}`
           )}
         </button>
-        {errorMsg && <div className="text-danger mt-2">{errorMsg}</div>}
+        {errorMsg && <div className="text-danger mt-3">{errorMsg}</div>}
       </form>
     </>
   );

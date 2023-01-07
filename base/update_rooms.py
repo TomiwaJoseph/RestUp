cf8446dc3021affbadcc3036a92a321908cb571c
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-07 09:09:32 +0100

    added logic to check and update expired rooms

--- base/update_rooms.py
+++ base/update_rooms.py
@@ -1,15 +1,22 @@
 from datetime import timedelta
 from django.utils import timezone
+from users.models import Booking
 
 
 def check_and_update_rooms():
+    # this function runs every day at 12:00PM
     print()
-    print("This function runs every day at 12:00PM")
-    # new_date = timezone.now() + timedelta(days=stay_duration)
-    # end_date = new_date.replace(hour=12, minute=00)
+    print("This function runs every 1 minutes")
+    # get all expired dates
+    all_expired_rooms = [obj.room for obj in Booking.objects.all(
+    ) if obj.end_date < timezone.now() and not obj.room.availability]
 
-    # print(end_date)
-    # print(type(end_date))
-    # print(end_date > timezone.now())
-    # print(end_date < timezone.now())
-    # print()
+    if all_expired_rooms:
+        # print(all_expired_rooms)
+        for obj in all_expired_rooms:
+            # print('changing the availability...')
+            obj.availability = True
+            obj.save()
+    else:
+        print('No dates are expired')
+    print()

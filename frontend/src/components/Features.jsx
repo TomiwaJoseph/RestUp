0dc4cbdff3f565494e6d34eff8826a4d07f2ab5b
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-16 04:28:19 +0200

    removed flaticon and added fontawesome

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -7,37 +7,37 @@ const Features = () => {
       <div className="row features-row">
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-restaurant"></i>
+            <i className="fas fa-utensils"></i>
             <p>Restaurant</p>
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-cup"></i>
+            <i className="fas fa-wine-glass"></i>
             <p>Bar</p>
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-car"></i>
+            <i className="fas fa-car"></i>
             <p>Pick-Up</p>
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-swimming"></i>
+            <i className="fas fa-swimmer"></i>
             <p>Pool</p>
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-massage"></i>
+            <i className="fas fa-spa"></i>
             <p>Spa</p>
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
-            <i className="flaticon-bicycle"></i>
+            <i className="fas fa-dumbbell"></i>
             <p>Gym</p>
           </div>
         </div>

0c333aeb33014ea15c43b291eac1aba6617b1281
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-01-19 12:47:50 +0200

    updated settings.py

--- RestUp/settings.py
+++ RestUp/settings.py
@@ -129,11 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static',
-    BASE_DIR / 'frontend/build/static/media',
 ]
 
 

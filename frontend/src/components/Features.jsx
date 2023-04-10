98ffcb76df2ada3cc118828bcef9359272af0956
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-09 23:41:49 -0700

    fixed features for mobile view

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -5,37 +5,37 @@ const Features = () => {
     <div className="container features-container">
       <hr />
       <div className="row features-row">
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-utensils"></i>
             <p>Restaurant</p>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-wine-glass"></i>
             <p>Bar</p>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-car"></i>
             <p>Pick-Up</p>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-swimmer"></i>
             <p>Pool</p>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-spa"></i>
             <p>Spa</p>
           </div>
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-4 icon-container text-center">
+        <div className="col-6 col-sm-6 col-md-4 col-lg-4 icon-container text-center">
           <div className="icon">
             <i className="fas fa-dumbbell"></i>
             <p>Gym</p>

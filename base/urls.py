321f3176dd4ade6656afd8aab08df8c1c17006d3
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-08-11 23:35:00 -0700

    added api endpoints the server should load from

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from base.views import index
+from django.urls import path, re_path
+from base.views import index, get_apartment, reserve_room
 
 urlpatterns = [
-    # path('', index)
+    path('', index),
+    path('apartments', index),
+    path('apartments/<slug:slug>', get_apartment),
+    path('reserve-room/<slug:apartment_slug>/<slug:room_slug>', reserve_room),
+    path('restaurant', index),
+    path('about', index),
+    path('login', index),
+    path('user/dashboard', index),
+    path('sign-up', index),
+    path('contact-us', index),
+    re_path(r'^.*/$', index),
 ]
